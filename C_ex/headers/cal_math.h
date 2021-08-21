28befb5670f0973ea83c8ae9beeaf57ca7fb02d9
kenonix <drone5023@naver.com>
2021-08-21 09:53:00 +0900

    코드정리(프로젝트 분리)

---
+++ C_ex/headers/cal_math.h
@@ -0,0 +1,5 @@
+#include "src\cal_math.c"
+int check_yun_year(int year);
+int first_day_of_month_gregrian(const int year, const int month, const int day);
+int first_day_of_month_julian(const int year, const int month, const int day);
+
