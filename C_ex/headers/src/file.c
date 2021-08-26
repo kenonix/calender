89ed6e413b4157ba59e4523c236aca0f2fc25c33
kenonix <drone5023@naver.com>
2021-08-26 17:56:01 +0900

    file read write error

--- C_ex/headers/src/file.c
+++ C_ex/headers/src/file.c
@@ -7,18 +7,21 @@ typedef struct __file_fomat{
   int start_day;
   int end_day;
   int contant_langth;
-  char contant[0];
+  char *contant;
 }file_fomat;
 
 int file_write(file_fomat *data){
   FILE * data_file;
   data_file = fopen("data.xc","wb");
-  fwrite(data, sizeof(*data), 1, data_file);
+  //test code up
+  printf("type: %d\n importance: %d\n start_day: %d\n end_day: %d\n contant_langth: %d\n contant: %s\n ",data->type,data->importance,data->start_day,data->end_day,data->contant_langth,data->contant);
+  //test code end
+  fwrite(data, sizeof(data), 1, data_file);
   fclose(data_file);
   return 0;
 }
 
-int flie_read(file_fomat *data){
+int file_read(file_fomat *data){
   FILE * data_file;
   data_file = fopen("data.xc","rb");
   if(data_file == NULL){
@@ -26,8 +29,8 @@ int flie_read(file_fomat *data){
     printf("ERROR code : %d\n",File_open_error);
     return File_open_error;
   }
-  data = malloc(sizeof(data));
-  file_fomat *read_data;
-  fread(read_data,sizeof(data),1,data_file);
+  printf("type: %d\n importance: %d\n start_day: %d\n end_day: %d\n contant_langth: %d\n contant: %s\n ",data->type,data->importance,data->start_day,data->end_day,data->contant_langth,data->contant);
+  data = malloc(sizeof(*data));
+  fread(data,sizeof(data),1,data_file);
   return 0;
 }
--- C_ex/tests/file-test.c
+++ C_ex/tests/file-test.c
@@ -1,9 +1,25 @@
-#include "../header/file.h"
-#include "../header/main.h"
+#include "../headers/file.h"
+#include "../headers/main.h"
 
-int main (){
+void w(){
   file_fomat d;
-  
-  file_writae(d);
-
+  d.type = 1;
+  d.importance = 0;
+  d.start_day = 0;
+  d.end_day = 0;
+  d.contant_langth = sizeof("just_test");
+  d.contant = (char*)malloc(sizeof(char)*(d.contant_langth));
+  sprintf_s((d.contant),((sizeof(char))*(d.contant_langth)),"just_test");
+  file_write(&d);
+  free(d.contant);
+}
+void r(){
+  file_fomat data;
+  file_read(&data);
+  printf("read data---------------\n");
+  printf("type: %d\n importance: %d\n start_day: %d\n end_day: %d\n contant_langth: %d\n contant: %s\n ",data.type,data.importance,data.start_day,data.end_day,data.contant_langth,data.contant);
+}
+int main (){
+  w();
+  r();
 }
