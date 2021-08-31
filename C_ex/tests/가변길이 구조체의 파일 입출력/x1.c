ebd6e7f0e050a3fdf78c6acc41a67ac4ea11557a
kenonix <drone5023@naver.com>
2021-08-31 17:59:34 +0900

    file up

--- C_ex/tests/가변길이 구조체의 파일 입출력/x1.c
+++ C_ex/tests/가변길이 구조체의 파일 입출력/x1.c
@@ -3,20 +3,25 @@
 
 typedef struct a{
 int num;
-char pup[];
+char *pup;
 }fid;
-fid * fix;
+fid *fix;
 void file_w(fid *data){
   FILE * data_file;
   data_file = fopen ("ddd.x1","wb");
   fwrite(data,sizeof(fid)+sizeof(data->num),1,data_file);
   fclose(data_file);
 }
+void file_r(fid *data){
+	FILE * data_file;
+	data_file = fopen ("ddd.xi","rb");
+	//fread(
+}
 
 int main (){
 
-fix->num = 20;
 fix = malloc(sizeof(fid)+sizeof(char)*fix->num);
+fix->num = 20;
 sprintf(fix->pup,"it's fucking work!");
 
 file_w(fix);
